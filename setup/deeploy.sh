591ba609cb83b09127274c761814718c137548cd
RiccardoGandolfi <riccardo.gandolfi@chips.it>
2025-10-13 07:24:21 +0000

    Added setup script for Deeploy

--- setup/deeploy.sh
+++ setup/deeploy.sh
@@ -3,10 +3,6 @@ DEEPLOY_ROOT="$ROOT"/Deeploy
 
 export MINIMALLOC_INSTALL_DIR=$DEEPLOY_ROOT/install/minimalloc
 export PULP_SDK_HOME=$ROOT/pulp-sdk
-export GVSOC_INSTALL_DIR=$ROOT/gvsoc/install
-export LLVM_INSTALL_DIR=/opt/riscv/snitch-llvm-15.0.0-snitch-0.1.0
+export GVSOC_INSTALL_DIR=$ROOT/pulp-sdk/install/workstation
+export LLVM_INSTALL_DIR=/opt/llvm
 export CMAKE=/usr/bin/cmake
-export PULP_OPEN_HOME=$ROOT
-export XTL_INSTALL_DIR=$DEEPLOY_ROOT/install/xtl
-export XSIMD_INSTALL_DIR=$DEEPLOY_ROOT/install/xsimd
-export XTENSOR_INSTALL_DIR=$DEEPLOY_ROOT/install/xtensor
