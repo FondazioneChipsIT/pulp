aad344e61d5ccc41b707c8544e391e5739497e4d
RiccardoGandolfi <riccardo.gandolfiu@chips.it>
2025-10-15 10:52:18 +0200

    Adjusted setup script for Deeploy

--- setup/deeploy.sh
+++ setup/deeploy.sh
@@ -3,6 +3,7 @@ DEEPLOY_ROOT="$ROOT"/Deeploy
 
 export MINIMALLOC_INSTALL_DIR=$DEEPLOY_ROOT/install/minimalloc
 export PULP_SDK_HOME=$ROOT/pulp-sdk
-export GVSOC_INSTALL_DIR=$ROOT/pulp-sdk/install/workstation
-export LLVM_INSTALL_DIR=/opt/llvm
+export GVSOC_INSTALL_DIR=$ROOT/gvsoc/install
+export LLVM_INSTALL_DIR=/opt/llvm_toolchain/llvm
 export CMAKE=/usr/bin/cmake
+export PATH=$ROOT/../../deeploy_gcc/gcc-11.4.0/bin/:$PATH
