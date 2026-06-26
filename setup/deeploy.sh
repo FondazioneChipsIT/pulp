a50808c8cd953d4e451efb7f61737e0747dd0b44
Riccardo Gandolfi <riccardogandi95@gmail.com>
2026-06-26 15:53:45 +0200

    [DEEPLOY] Add GCC_INSTALL_DIR variable to deeploy setup script

--- setup/deeploy.sh
+++ setup/deeploy.sh
@@ -5,6 +5,7 @@ export MINIMALLOC_INSTALL_DIR=$DEEPLOY_ROOT/install/minimalloc
 export PULP_SDK_HOME=$ROOT/pulp-sdk
 export GVSOC_INSTALL_DIR=$ROOT/gvsoc/install
 export LLVM_INSTALL_DIR=/opt/llvm_toolchain/llvm
+export GCC_INSTALL_DIR=/opt/riscv/pulp-gcc-1.0.16/
 export CMAKE=/usr/bin/cmake
 export PATH=$ROOT/../../deeploy_gcc/gcc-11.4.0/bin/:$PATH
 export PULP_OPEN_HOME=$ROOT
